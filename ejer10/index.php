574d93ef2bf7ee158d1add1ee9e1a73c21998c54
Alice <herrada.aliciab@gmail.com>
2012-03-20 09:28:15 -0300

    first commit

---
+++ ejer10/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title> JavaScrip Mobile Page </title>
+        
+        <script src="http://code.jquery.com/jquery-1.7.1.min.js"> </script>
+        <script src="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.js"></script>
+        
+    </head>
+    <body>
+        
+        <br>
+        
+        <!-- 10.Load the content of pages/sample1.php in a new page  -->
+        
+        <a href="sample1.php" target="_blank"> Go to Sample1 </a>
+        
+    </body>
+</html>
---
+++ ejer10/sample1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Sample page 1</title>
+    </head>
+    <body>
+        <header>
+            <h1>This is Sample page 1</h1>
+        </header>
+
+        <div>
+            OK, if you're seeing this inside a jQuery Mobile page...<br />
+            <strong>You did it!</strong><br />
+            :D
+        </div>
+    </body>
+</html>
