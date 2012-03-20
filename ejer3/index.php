66f030eb332811290fc376182612f51d7005acde
Alice <herrada.aliciab@gmail.com>
2012-03-20 08:58:10 -0300

    first commit

---
+++ ejer3/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <!-- 3.Set viewport -->
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- ************** -->
+        
+        <title> JavaScript Mobile Page </title>
+        
+        <script src="http://code.jquery.com/jquery-1.7.1.min.js"> </script>
+        <script src="http://code.jquery.com/mobile/1.1.0-rc.1/jquery.mobile-1.1.0-rc.1.min.js"></script>
+        
+    </head>
+    <body>
+        
+        <br> Mobile page with viewport
+        
+    </body>
+</html>
